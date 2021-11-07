74ea876105fad84a0d4cac364473cfa45a6c57b8
Àlex Costa Sánchez <alex.costasanchez@gmail.com>
2021-11-07 16:51:32 +0100

    feat: implement jaccard similarity

--- lab1/Shingling.py
+++ lab1/Shingling.py
@@ -4,15 +4,20 @@ class Shingling:
     def __init__(self,k):
         self.k = k
 
-    def shingle_document(self,document):
+    def shingle_document(self, document):
         shingle = []
         for i in range(len(document)-self.k):
             shingle.append(document[i:i+self.k])
         shingle = list(set(shingle))
-        print(len(shingle))
+        shingle = list(map(self.hash, shingle))
+        shingle.sort()
+        return shingle
+
+    def hash(self, shingle):
+        return hash(shingle) % 2**32
 
 if __name__=="__main__":
-    f = open("data/2537newsML.txt", "r")
+    f = open("data/225900newsML.txt", "r")
     document = f.read()
     shing = Shingling(5)
-    shing.shingle_document(document)
+    print(shing.shingle_document(document))
