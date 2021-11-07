483207c5a97ec01c1087b173342fe0e8c23de09a
annasanchez27 <annasanchezespunyes27@gmail.com>
2021-11-07 17:22:41 +0100

    test jaccard similarity

--- lab1/Shingling.py
+++ lab1/Shingling.py
@@ -1,23 +1,32 @@
+from CompareSet import CompareSet
 
 
 class Shingling:
-    def __init__(self,k):
+    def __init__(self, k):
         self.k = k
 
     def shingle_document(self, document):
         shingle = []
-        for i in range(len(document)-self.k):
-            shingle.append(document[i:i+self.k])
+        for i in range(len(document) - self.k):
+            shingle.append(document[i:i + self.k])
         shingle = list(set(shingle))
         shingle = list(map(self.hash, shingle))
         shingle.sort()
         return shingle
 
     def hash(self, shingle):
-        return hash(shingle) % 2**32
+        return hash(shingle) % 2 ** 32
 
-if __name__=="__main__":
+
+if __name__ == "__main__":
     f = open("data/225900newsML.txt", "r")
-    document = f.read()
-    shing = Shingling(5)
-    print(shing.shingle_document(document))
+    doc = f.read()
+    shing = Shingling(2)
+    hashed_shing1 = shing.shingle_document(doc)
+
+    f2 = open("data/120600newsML.txt", "r")
+    doc2 = f2.read()
+    hashed_shing2 = shing.shingle_document(doc2)
+
+    compare = CompareSet()
+    print(compare.jaccard_similarity(hashed_shing1, hashed_shing2))
