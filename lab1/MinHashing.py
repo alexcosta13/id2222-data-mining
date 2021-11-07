bef55b214b01e6be75a1bb64abb8fd74e29d6c65
Àlex Costa Sánchez <alex.costasanchez@gmail.com>
2021-11-07 17:42:32 +0100

    feat: implement minHashing signature

---
+++ lab1/MinHashing.py
@@ -0,0 +1,17 @@
+import random
+
+class MinHashing:
+    def __init__(self, n):
+        self.n = n
+        self.a = [random.randint(0, n ** n) for _ in range(n)]
+        self.b = [random.randint(0, n ** n) for _ in range(n)]
+        # TODO c has to be prime?
+        self.c = 2**32
+
+    def compute_signature(self, shingles):
+        signature = []
+        for i in range(self.n):
+            hashed = []
+            for shingle in shingles:
+                hashed.append((self.a[i] * shingle + self.b[i]) % self.c)
+            signature.append(min(hashed))
