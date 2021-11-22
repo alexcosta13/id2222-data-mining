2dfdb90c7278337fd582de8e6e2bb9ab455d7359
Àlex Costa Sánchez <alex.costasanchez@gmail.com>
2021-11-22 13:55:04 +0100

    feat: add experiments

--- lab2/apriori.py
+++ lab2/apriori.py
@@ -1,10 +1,7 @@
-from itertools import combinations, permutations
-from tqdm import tqdm
-import time
+import itertools
 
 
 class Apriori:
-
     def count_singletons(self, baskets):
         count = {}
         for basket in baskets:
@@ -15,35 +12,21 @@ class Apriori:
                     count[elem] = 1
         return count
 
-    def count_tuples_fast(self, baskets, frequent_items):
-        item_length = len(frequent_items[0])
-        count = {}
-        for basket in tqdm(baskets):
-            basket_combinations = combinations(basket, item_length)
-            for c in basket_combinations:
-                if c in frequent_items:
-                    if c in count:
-                        count[c] += 1
-                    else:
-                        count[c] = 1
-        return count
-
-    def generate_candidates(self, items, singletons):
+    def make_combinations(self, item_sets, frequent_items):
         candidates = {}
-        for item in items:
-            for singleton in singletons:
-                if singleton not in item:
-                    candidate = tuple(sorted(item + (singleton, )))
-                    if candidate not in candidates:
-                        candidates[candidate] = 0
+        for item in item_sets:
+            for single_item in frequent_items:
+                if single_item not in item:
+                    candidate = tuple(sorted(item + (single_item,)))
+                    candidates[candidate] = 0
         return candidates
 
-    def count_candidates(self, baskets, candidates):
-        count = candidates.copy()
-        candidates_length = len(list(candidates.keys())[0])
+    def count_tuples(self, baskets, combinations):
+        count = combinations.copy()
+        candidates_length = len(list(combinations.keys())[0])
         for basket in baskets:
-            basket_variations = combinations(basket, candidates_length)
-            for combination in basket_variations:
+            basket_combinations = itertools.combinations(basket, candidates_length)
+            for combination in basket_combinations:
                 if combination in count:
                     count[combination] += 1
         return count
@@ -51,39 +34,16 @@ class Apriori:
     def filter(self, count, support):
         return {key: value for key, value in count.items() if value >= support}
 
-    def check_sublist(self, value, item):
-        comb = []
-        for i in combinations(value, len(value)-1):
-            i_list = list(i)
-            i_list.append(item)
-            comb.append(tuple(i_list))
-        return set(comb)
-
-    def make_combinations(self, singletons, item_sets):
-        updated_set = []
-        for value in tqdm(item_sets):
-            for singleton in singletons:
-                if singleton not in value:
-                    value_list = list(value)
-                    value_list.append(singleton)
-                    value_list.sort()
-                    if self.check_sublist(value, singleton).issubset(item_sets):
-                        updated_set.append(tuple(value_list))
-
-        return list(set(updated_set))
-
     def aprori_algorithm(self, baskets, support):
         count_items = self.count_singletons(baskets)
         acc_count = self.filter(count_items, support)
         frequent_items = acc_count.keys()
         item_sets = [(item,) for item in frequent_items]
-        acc_count = {(key, ): value for key, value in acc_count.items()}
+        acc_count = {(key,): value for key, value in acc_count.items()}
 
         while len(item_sets) > 0:
-            #combinations = self.make_combinations(frequent_items, item_sets)
-            #tuples_count = self.count_tuples_fast(baskets, combinations)
-            candidates = self.generate_candidates(item_sets, frequent_items)
-            tuples_count = self.count_candidates(baskets, candidates)
+            combinations = self.make_combinations(item_sets, frequent_items)
+            tuples_count = self.count_tuples(baskets, combinations)
             count = self.filter(tuples_count, support)
             item_sets = count.keys()
             acc_count.update(count)
@@ -95,18 +55,17 @@ class Apriori:
         frequent_set = count.keys()
         for itemset in frequent_set:
             if len(itemset) > 1:
-                candidates = permutations(itemset)
+                candidates = itertools.permutations(itemset)
                 for rule in candidates:
-                    for i in range(len(itemset)-1, 0, -1):
+                    for i in range(len(itemset) - 1, 0, -1):
                         num_idx = tuple(sorted(list(rule)))
                         den_idx = tuple(sorted(list(rule[:i])))
                         i_idx = tuple(sorted(list(rule[i:])))
                         conf = count[num_idx] / count[den_idx]
                         if conf > confidence:
-                            key = f"{', '. join(list(map(str, i_idx)))} --> {', '. join(list(map(str, den_idx)))}"
+                            key = f"{', '. join(list(map(str, den_idx)))} ==> {', '. join(list(map(str, i_idx)))}"
                             associations[key] = conf
                         else:
                             break
 
         return associations
-
--- lab2/main.py
+++ lab2/main.py
@@ -1,18 +1,11 @@
 import argparse
+import json
 from time import time
 
 from apriori import Apriori
 
 
-def support(args):
-    with open('data/T10I4D100K.dat') as f:
-        lines = f.read().splitlines()
-
-    baskets = []
-    for line in lines:
-        line = line.strip()
-        baskets.append(list(map(int, line.split(' '))))
-
+def support(args, baskets):
     apriori = Apriori()
     count = apriori.aprori_algorithm(baskets, args.support)
     print(count)
@@ -21,24 +14,78 @@ def support(args):
     print(rules)
 
 
+def support_experiment(args, baskets):
+    apriori = Apriori()
+
+    for s in [1000, 750, 500, 250]:
+        start = time()
+        count = apriori.aprori_algorithm(baskets, s)
+        end = time()
+        items = count.keys()
+        length = len([item for item in items if len(item) == 1])
+
+        print(f"A priori algorithm with support {s} took {end-start}s.")
+
+        i = 1
+        while length > 0:
+            print(f"There are {length} frequent items of length {i} (support {s}).")
+            i += 1
+            length = len([item for item in items if len(item) == i])
+
+
+def confidence_experiment(args, baskets):
+    apriori = Apriori()
+
+    for s in [1000, 900, 800]:
+        count = apriori.aprori_algorithm(baskets, s)
+
+        for c in [0.9, 0.8, 0.7]:
+            rules = apriori.generate_association_rules(count, c)
+            print(
+                f"Generating associations rules with support {s} and confidence {c} yields {len(rules)} rules:"
+            )
+            print(json.dumps(rules, indent=2))
+
+
 def main():
+    with open("data/T10I4D100K.dat") as f:
+        lines = f.read().splitlines()
+
+    baskets = []
+    for line in lines:
+        line = line.strip()
+        baskets.append(list(map(int, line.split(" "))))
+
     parser = argparse.ArgumentParser(description="INSERT DESCRIPTION.")
     parser.add_argument(
-        "--support", "-s",
+        "--support",
+        "-s",
         type=int,
         default=1000,
         help="support (default: 1000)",
     )
     parser.add_argument(
-        "--confidence", "-c",
+        "--confidence",
+        "-c",
         type=float,
         default=0.5,
         help="confidence (default: 0.5)",
     )
+    parser.add_argument(
+        "--s_experiment", dest="s_experiment", default=False, action="store_true"
+    )
+    parser.add_argument(
+        "--c_experiment", dest="c_experiment", default=False, action="store_true"
+    )
 
     args = parser.parse_args()
 
-    support(args)
+    if args.s_experiment:
+        support_experiment(args, baskets)
+    elif args.c_experiment:
+        confidence_experiment(args, baskets)
+    else:
+        support(args, baskets)
 
 
 if __name__ == "__main__":
