b227e50edb17a089a22ea29984b95fe9f6401b1f
Àlex Costa Sánchez <alex.costasanchez@gmail.com>
2021-12-05 13:16:18 +0100

    feat: add plots and Fiedler vector representation

--- lab4/main.py
+++ lab4/main.py
@@ -1,21 +1,85 @@
+import networkx as nx
+import matplotlib.pyplot as plt
+import numpy as np
+
 from spectralclustering import SpectralClustering
 
+
+def draw_graph(graph_name, labels=None):
+    graph = nx.Graph()
+    filename = "data/" + graph_name + ".dat"
+    edge_list = nx.read_edgelist(
+        filename, delimiter=",", nodetype=int, data=(("weight", int),)
+    )
+    graph.add_nodes_from(sorted(edge_list.nodes.keys()))
+    graph.add_edges_from(edge_list.edges)
+    title = f"{graph_name}" if labels is None else f"Classified {graph_name}"
+    plt.title(title)
+    nx.draw(graph, node_size=30, node_color=labels, edge_color="grey")
+    plt.show()
+
+
+def show_plots(data):
+    draw_graph(data["graph"])
+    draw_graph(data["graph"], data["labels"])
+    if data["affinity"] is not None:
+        plt.spy(data["affinity"])
+        plt.title("Affinity matrix")
+        plt.show()
+    if data["eigenvalues"] is not None:
+        plt.plot(
+            range(1, len(data["eigenvalues"]) + 1), sorted(data["eigenvalues"])[::-1]
+        )
+        plt.title("Sorted eigenvalues")
+        plt.show()
+    if data["fiedler_vector"] is not None:
+        plt.plot(data["fiedler_vector"])
+        plt.title("Fiedler vector")
+        plt.show()
+
+
 def main():
-    with open("data/example1.dat") as f:
-        lines = f.read().splitlines()
+    for graph in ["example1", "example2"]:
+        filename = "data/" + graph + ".dat"
+        with open(filename) as f:
+            lines = f.read().splitlines()
 
-    edges = []
-    for line in lines:
-        line = line.strip()
-        edges.append(tuple(map(int, line.split(","))))
+        edges = []
+        for line in lines:
+            line = line.strip()
+            edges.append(tuple(map(int, line.split(",")))[:2])
 
-    spectral_clust = SpectralClustering()
-    affinity_matrix = spectral_clust.get_affinity_matrix(edges)
-    diagonal_matrix = spectral_clust.get_diagonal(affinity_matrix)
-    laplacian = spectral_clust.get_laplacian(diagonal_matrix, affinity_matrix)
-    x_matrix = spectral_clust.find_largest_eigenvectors(laplacian,10)
-    norm_x = spectral_clust.normalize_x(x_matrix)
-    spectral_clust.k_means(norm_x, 10)
+        spectral_clustering = SpectralClustering()
+        affinity_matrix = spectral_clustering.get_affinity_matrix(edges)
+        diagonal_matrix = spectral_clustering.get_diagonal(affinity_matrix)
+        laplacian = spectral_clustering.get_laplacian(diagonal_matrix, affinity_matrix)
+        (
+            laplacian_eigenvalues,
+            laplacian_eigenvectors,
+        ) = spectral_clustering.find_eigenvectors(laplacian)
+        K = spectral_clustering.optimal_k(laplacian_eigenvalues)
+        x_matrix = laplacian_eigenvectors[:, -K:]
+        norm_x = spectral_clustering.normalize_x(x_matrix)
+        labels = spectral_clustering.k_means(norm_x, K)
+
+        (
+            u_laplacian_eigenvalues,
+            u_laplacian_eigenvectors,
+        ) = spectral_clustering.find_eigenvectors(diagonal_matrix - affinity_matrix)
+        fiedler_vector = spectral_clustering.get_fiedler_vector(
+            u_laplacian_eigenvalues, u_laplacian_eigenvectors
+        )
+        fiedler_vector = np.sort(np.array(fiedler_vector).squeeze())
+
+        data = {
+            "graph": graph,
+            "labels": labels,
+            "affinity": affinity_matrix,
+            "eigenvalues": laplacian_eigenvalues,
+            "fiedler_vector": fiedler_vector,
+        }
+
+        show_plots(data)
 
 
 if __name__ == "__main__":
--- lab4/spectralclustering.py
+++ lab4/spectralclustering.py
@@ -2,6 +2,7 @@ import numpy as np
 from scipy.sparse import csr_matrix
 from sklearn.cluster import KMeans
 
+
 class SpectralClustering:
     def __init__(self):
         pass
@@ -11,8 +12,9 @@ class SpectralClustering:
         list_nodes_v = [v - 1 for u, v in edges]
         max_node = max(max(list_nodes_u, list_nodes_v))
         ones = np.ones(len(list_nodes_u))
-        return csr_matrix((ones, (list_nodes_u, list_nodes_v)),
-                          shape=(max_node + 1, max_node + 1)).todense()
+        return csr_matrix(
+            (ones, (list_nodes_u, list_nodes_v)), shape=(max_node + 1, max_node + 1)
+        ).todense()
 
     def get_diagonal(self, affinity_matrix):
         diag_values = np.sum(affinity_matrix, axis=1)
@@ -23,16 +25,32 @@ class SpectralClustering:
         inv = np.linalg.inv(np.sqrt(diagonal_matrix))
         return inv @ affinity_matrix @ inv
 
-    def find_largest_eigenvectors(self, laplacian_matrix, k):
+    def find_eigenvectors(self, laplacian_matrix):
         # eigh only works for real symmetric matrix
-        _, eigenvectors = np.linalg.eigh(laplacian_matrix)
-        return eigenvectors[:, -k:]
+        eigenvalues, eigenvectors = np.linalg.eigh(laplacian_matrix)
+        return eigenvalues, eigenvectors
 
     def normalize_x(self, x_matrix):
-        denom = (np.sqrt(np.sum(np.square(x_matrix), axis=1))).reshape(-1,1)
+        denom = (np.sqrt(np.sum(np.square(x_matrix), axis=1))).reshape(-1, 1)
         return x_matrix / denom
 
     def k_means(self, list_points, k):
         kmeans = KMeans(n_clusters=k).fit(list_points)
-        labels = kmeans.labels_
-        print(labels)
+        return kmeans.labels_
+
+    def optimal_k(self, eigenvalues):
+        eigenvalues = sorted(eigenvalues)[::-1]
+        delta = [
+            eigenvalues[i] - eigenvalues[i + 1] for i in range(len(eigenvalues) - 1)
+        ]
+        max_difference = max(delta)
+        max_index = delta.index(max_difference)
+        return max_index + 1
+
+    def get_fiedler_vector(self, eigenvalues, eigenvectors):
+        second_smallest_element = eigenvalues[1]
+        idx = np.where(eigenvalues == second_smallest_element)[0][0]
+        return eigenvectors[:, idx]
+
+    def unnormalize_laplacian(self, diagonal, affinity):
+        return diagonal - affinity
